caf7f3937f75897bf625d8b29316b992714ac426
ChampagneCui <xbcui2010@hotmail.com>
2016-09-26 23:17:12 +0800

    day3 final

--- day3/hw/haproxy.py
+++ day3/hw/haproxy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2.7
 # _*_ coding:utf-8 _*_
-#目前遗留问题，修改的时候，如果修改的新的值是重复的则有bug
 import os
 
 conf='haproxy.cfg'
@@ -18,7 +17,7 @@ def welcome():
     ''')
 
 
-def choise():
+def choise(): #选项
     num=raw_input('请输入操作序号：')
     while num.isdigit() != True:
         num=raw_input('请输入操作序号：')
@@ -27,7 +26,7 @@ def choise():
         num = raw_input('请输入操作序号：')
     haproxy_conf(num)
 
-def haproxy_conf(num):
+def haproxy_conf(num): #根据选项调度
     if num==1:
         backend=raw_input('Which backend do you want to see?')
         feature1(backend)
@@ -45,7 +44,7 @@ def haproxy_conf(num):
         b = raw_input('Please enter the server information:')
         feature4(a, b)
 
-def public(backend): #查看指定backend信息，思路：遇到匹配的行就把flag设为打印，然后再次遇到空行就flag设为结束
+def public(backend): #查看指定backend信息，思路：遇到匹配的行就把flag设为打印，然后遇到空行就flag设为结束
     global result
     result=[]
     with open(conf) as f1:
@@ -59,7 +58,7 @@ def public(backend): #查看指定backend信息，思路：遇到匹配的行就
                 print_flag=0
     return result
 
-def if_server_exist(server):
+def if_server_exist(server): #查看server_ip是否存在
     global server_ip
     result_list = []
     for i in result:
@@ -104,26 +103,29 @@ def feature2(backend,server): #添加，思路：如果backend已有就加在现
                 f2.write(server_temp)
         mv()
 
-def feature3(backend,server,new_server):#修改
+def feature3(backend,server,new_server):#修改，核心思想：先用if_server_exist判断存不存在，如果存在则在指定backend后遇到的第一个相同server ip处continue掉
     flag=1
     if public(backend)!=[]: #backend存在
-        if if_server_exist(server)==0:
-            with open(conf) as f1:
-                with open(new_conf, 'w') as f2:
-                    for line in f1:
-                        if line.find('backend %s' % (backend)) >= 0:
-                            flag=0 #关键
-                        if flag==0 and line.find(server_ip)>=0:
-                            flag=1
-                            line = ('%s %s \n') % (blank,new_server)
-                        f2.write(line)
-            mv()
+        if if_server_exist(new_server)==0:
+            print('New server ip already existed!')
         else:
-            print('No such server in backend:%s') %(backend)
+            if if_server_exist(server)==0:
+                with open(conf) as f1:
+                    with open(new_conf, 'w') as f2:
+                        for line in f1:
+                            if line.find('backend %s' % (backend)) >= 0:
+                                flag=0 #关键
+                            if flag==0 and line.find(server_ip)>=0:
+                                flag=1
+                                line = ('%s %s \n') % (blank,new_server)
+                            f2.write(line)
+                mv()
+            else:
+                print('No such server in backend:%s') %(backend)
     else:
         print('No such backend!')
 
-def feature4(backend,server):#删除
+def feature4(backend,server):#删除，核心思想与上面修改累死
     flag=1
     if public(backend) != []:  # backend存在
         if if_server_exist(server)==0:
