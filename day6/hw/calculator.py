66dbfe681e60cc89d21ea4433b51994ec86fbd48
ChampagneCui <xbcui2010@hotmail.com>
2016-10-17 23:47:55 +0800

    day6

--- day6/hw/calculator.py
+++ day6/hw/calculator.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
+import re
+
+def cal(req):
+    '''乘除加减'''
+    req=req.strip('(')
+    req = req.strip(')')
+    res=eval(req)
+    return res
+
+def deal():
+    #expression=raw_input('请输入表达式：')
+    expression='1+2*(2+(3+2))+2*(3+4)'
+    expression='('+expression+')'
+    #while 1:
+    a=re.findall('\([^()]*\)',expression)
+    a=a[0]
+    b=str(cal(a))
+    expression=re.sub(a,b,expression,1)
+    print expression
+
+deal()
 
