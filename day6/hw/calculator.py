e44a7c568303043ea38d0b99944322afee3111c3
cuixiangbin <cuixiangbin@limei.com>
2016-10-18 13:54:09 +0800

    day6

--- day6/hw/calculator.py
+++ day6/hw/calculator.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
+from __future__ import division
 import re
+
+
+def deal_a(req):
+    '''re.sub替换前加转义符'''
+    for i in req:
+        res=re.sub('\(','\\(',req)
+        res = re.sub('\)', '\\)', res)
+        res = re.sub('\+', '\\+', res)
+        res = re.sub('\-', '\\-', res)
+        res = re.sub('\*', '\\*', res)
+        res = re.sub('\/', '\\/', res)
+        return res
 
 def cal(req):
     '''乘除加减'''
@@ -10,15 +23,20 @@ def cal(req):
     return res
 
 def deal():
-    #expression=raw_input('请输入表达式：')
-    expression='1+2*(2+(3+2))+2*(3+4)'
+    expression=raw_input('请输入表达式：')
     expression='('+expression+')'
-    #while 1:
-    a=re.findall('\([^()]*\)',expression)
-    a=a[0]
-    b=str(cal(a))
-    expression=re.sub(a,b,expression,1)
+    while '('  in expression:
+        if ('(' in expression and ')' not in expression) or ('(' not in expression and ')' in expression):
+            exit('Input error!')
+        else:
+            a=re.findall('\([^()]*\)',expression)
+            a=a[0]
+            b=str(cal(a))
+            a=deal_a(a)
+            expression=re.sub(a,b,expression,1)
+            #print expression
     print expression
 
-deal()
 
+if __name__ == '__main__':
+    deal()
