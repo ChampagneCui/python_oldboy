8c7b7792b725c22c8fa7631134417f8f36646d54
cuixiangbin <cuixiangbin@limei.com>
2016-10-24 17:18:48 +0800

    day7

--- day7/hw/bin/courses.py
+++ day7/hw/bin/courses.py
@@ -4,4 +4,24 @@ from sys import path
 path.append(r'..\modules')
 path.append('..\conf')
 
-class 
+class teachers:
+    def __init__(self,favour,name,age):
+        self.favour=favour
+        self.name=name
+        self.age=age
+        self.asset=0
+
+    def accident(self):
+
+    def gain(self):
+
+class courses:
+    def __init__(self,name,fee,teacher):
+
+
+    def lesson(self):
+
+
+class student:
+    def money(self):
+        def __init__(self, favour, name, age):
