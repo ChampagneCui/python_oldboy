351e9192ec8c2f8102bdd19f2ccb8d397af6b1a8
cuixiangbin <cuixiangbin@limei.com>
2016-10-25 18:02:27 +0800

    day7

--- day7/hw/bin/courses.py
+++ day7/hw/bin/courses.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
+import pickle
 from sys import path
 path.append(r'..\modules')
 path.append('..\conf')
 
 class teachers:
-    def __init__(self,favour,name,age):
+    def __init__(self):
         favour = raw_input('favour:')
         name = raw_input('name:')
         age = raw_input('age:')
@@ -26,13 +27,11 @@ class teachers:
     def gain(self,fee):
         self.asset = self.asset + fee
 
-
-
 class courses:
     def __init__(self,name,fee,teacher):
         name = raw_input('Please enter a course you want create:')
         fee = raw_input('How much:')
-        teacher = raw_input('Which teacher?')#可用购物车的形式选择
+        teacher = raw_input('Which teacher?')
         self.name = name
         self.fee=fee
         self.teacher=teacher
@@ -43,8 +42,6 @@ class courses:
 
 class students:
         def __init__(self,name, age):
-            name = raw_input('name:')
-            age = raw_input('age:')
             self.name=name
             self.age=age
             self.asset=0
@@ -57,6 +54,14 @@ class students:
             self.experience.append(course)
             self.asset-=fee
 
+#
+a=teachers()
+a.print_info()
+f=file('1.txt','w')
+pickle.dump(a,f)
+f.close()
 
-
-
+f=file('1.txt')
+b=pickle.load(f)
+f.close()
+b.print_info()
