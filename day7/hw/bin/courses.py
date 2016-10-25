8d65c1e5f4772697a23b3cb40ff87d7230e26a4d
cuixiangbin <cuixiangbin@limei.com>
2016-10-25 14:47:42 +0800

    day7

--- day7/hw/bin/courses.py
+++ day7/hw/bin/courses.py
@@ -12,16 +12,33 @@ class teachers:
         self.asset=0
 
     def accident(self):
+        self.asset=self.asset-5
 
-    def gain(self):
+    def gain(self,fee):
+        self.asset = self.asset + fee
 
 class courses:
     def __init__(self,name,fee,teacher):
+        self.name = name
+        self.fee=fee
+        self.teacher=teacher
 
-
-    def lesson(self):
-
+    def lesson(self,experience,course,fee):
+        teachers.gain(fee)
+        student.st_add_experience(course,fee)
 
 class student:
-    def money(self):
-        def __init__(self, favour, name, age):
+        def __init__(self,name, age):
+            self.name=name
+            self.age=age
+            self.asset=0
+            self.experience=[]
+
+
+        def st_add_money(self,money):
+            self.asset+=money
+
+        def st_add_experience(self,course,fee):
+            self.experience.append(course)
+            self.asset-=fee
+
