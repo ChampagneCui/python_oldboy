395045f77828872e394506e308972951e82865fd
ChampagneCui <xbcui2010@hotmail.com>
2016-11-13 12:20:12 +0800

    day7

--- day7/hw/modules/course.py
+++ day7/hw/modules/course.py
@@ -32,29 +32,44 @@ def s_main():
                 print i
             course_name=raw_input('Press the course you want！')
             #if_exist_course
-            f=COURSES_DB_PATH+course_name
-            course=pickle.load(open(f))
-            student.choose_course(course)
-            pickle.dump(student,open(student_db_file,'w'))
+            if course_name in os.listdir(COURSES_DB_PATH):
+                f=COURSES_DB_PATH+course_name
+                course=pickle.load(open(f))
+                student.choose_course(course)
+                pickle.dump(student,open(student_db_file,'w'))
+            else:
+                print('Wrong entering!')
         elif choose == '2':#上课
             for num,i in enumerate(student.course):
                 print(num,i.name)
             course_num = raw_input('Press the number you want！')
-            #if_exist_course
-            student.have_lesson(course_num)
-            f=STUDENTS_DB_PATH+student.name
-            pickle.dump(student,open(f,'w'))
-            # teacher_name=student.course[int(course_num)].teacher.name
-            # teacher_path=TEACHERS_DB_PATH+teacher_name
-            # pickle.dump(teacher,open(teacher_path,'w'))
+            if int(course_num) <= (len(student.course)-1):
+                #if has money
+                student.have_lesson(course_num)
+                f=STUDENTS_DB_PATH+student.name
+                pickle.dump(student,open(f,'w'))
+                teacher_name=student.course[int(course_num)].teacher.name
+                teacher_path=TEACHERS_DB_PATH+teacher_name
+                pickle.dump(student.course[int(course_num)].teacher,open(teacher_path,'w'))
+            else:
+                print('No this course!')
         elif choose == '3':#查看历史
             print(student.experience)
         elif choose == '4':#充值
             money=raw_input('How much do you want to add?')
-            #check_num(money)
-            student.st_add_money(int(money))
-            pickle.dump(students,open(student_db_file,'w'))
+            while check_num(money)==1:
+                student.st_add_money(int(money))
+                pickle.dump(students,open(student_db_file,'w'))
 
+
+
+def check_num(num):
+    num=str(num)
+    if num.isdigit()==True:#用digit函数判断
+        return 0
+    else:
+        print('Please enter a right number!')
+        return 1
 
 def m_main():
     while 1:
@@ -77,18 +92,3 @@ def m_main():
             pickle.dump(course,open(f,'w'))
 
 
-#
-# #
-f=STUDENTS_DB_PATH+'leo'
-a=pickle.load(open(f))
-print(a.name,a.experience,a.asset,a.course[0].teacher.asset)
-#
-
-#
-f=TEACHERS_DB_PATH+'kk'
-a=pickle.load(open(f))
-print(a.name,a.asset)
-
-f=COURSES_DB_PATH+'music'
-a=pickle.load(open(f))
-print(a.name,a.fee,a.teacher.name)
