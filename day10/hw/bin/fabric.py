ff693539c4eb4ac7a8e5bb2429780f51f7a69156
ChampagneCui <xbcui2010@hotmail.com>
2017-01-16 23:25:01 +0800

    day10

--- day10/hw/bin/fabric.py
+++ day10/hw/bin/fabric.py
@@ -10,17 +10,25 @@ import sys
 import getopt
 
 if __name__ == '__main__':
-    opts, args = getopt.getopt(sys.argv[1:], "ha:b:c:")
-    if len(opts) == 1:
-        for op, value in opts:
-            if (op == "-a"):
-                aa = value
-            elif (op == "-p"):
-                aa = value
-            elif (op == "-g"):
-                aa = value
+    #
+    #fab --ip=leo@192.168.1.1:2299 --command="shell"
+    #fab --ip=192.168.1.1. --command="put" --src="/tmp" --dest="/tmp"
+    opts, args = getopt.getopt(sys.argv[1:],longopts =["help","host"=,"command=","src=","dest="])
+    for op, value in opts:
+            if (op == "--help"):
+                usage() #help说明
+            elif (op == "--host"):
+                #用正则匹配@：，等，没有则给予默认
+            elif (op == "--command"):
+                command=value
+            elif (op == "--src"):
+                src=value
+            elif (op == "--dest"):
+                dest= value
 
-        print(aa)
+    if command=='shell':
+
+
     # if choice[0]=='show':
     #     print(dic.keys())
     # elif choice[0] == 'get':
