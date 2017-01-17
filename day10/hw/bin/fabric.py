3632e718326104322a6859438865f7bd78b311c5
user <user>
2017-01-17 16:45:03 +0800

    day10

--- day10/hw/bin/fabric.py
+++ day10/hw/bin/fabric.py
@@ -10,14 +10,18 @@ import sys
 import getopt
 
 if __name__ == '__main__':
-    #fab --ip=leo@192.168.1.1:2299 --command="shell"
-    #fab --ip=192.168.1.1. --command="put" --src="/tmp" --dest="/tmp"
+    #fab --host=leo@192.168.1.1:2299 --command="shell"
+    #fab --host=192.168.1.1. --command="put" --src="/tmp" --dest="/tmp"
     opts, args = getopt.getopt(sys.argv[1:],longopts =["help","host=","mode=","src=","dest=","command="])
-    for op, value in opts:
-            if (op == "--help"):
-                usage() #help说明
-            elif (op == "--host"):
-                #用正则匹配@：，等，没有则给予默认
+    for op, value in opts:'
+            if (op == "--host"):
+                '''用正则匹配@：等，没有则给予默认'''
+                if value.find('@'):
+                    username,value=value.split('@')
+                if value.find(':')
+                    hostname,port=value.split(':')
+                    continue
+                hostname=value
             elif (op == "--mode"):
                 mode=value
             elif (op == "--src"):
@@ -26,13 +30,15 @@ if __name__ == '__main__':
                 dest= value
             elif (op == "--command"):
                 command=value
+            else:
+                usage() #help说明
 
     if mode=='do':
-        ssh_command(#host,command)
+        ssh_command(hostname,command)
     elif mode=='get':
-        get(#host,src)
+        get(hostname,src)
     elif mode=='put':
-        put(#host,src,dest)
+        put(hostname,src,dest)
     elif mode=='show':
         ####
 
