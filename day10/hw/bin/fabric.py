86a69d50d0e23a93c9fd581b3d3032d1858a2c9b
user <user>
2017-01-17 16:17:56 +0800

    day10

--- day10/hw/bin/fabric.py
+++ day10/hw/bin/fabric.py
@@ -10,30 +10,29 @@ import sys
 import getopt
 
 if __name__ == '__main__':
-    #
     #fab --ip=leo@192.168.1.1:2299 --command="shell"
     #fab --ip=192.168.1.1. --command="put" --src="/tmp" --dest="/tmp"
-    opts, args = getopt.getopt(sys.argv[1:],longopts =["help","host"=,"command=","src=","dest="])
+    opts, args = getopt.getopt(sys.argv[1:],longopts =["help","host=","mode=","src=","dest=","command="])
     for op, value in opts:
             if (op == "--help"):
                 usage() #help说明
             elif (op == "--host"):
                 #用正则匹配@：，等，没有则给予默认
-            elif (op == "--command"):
-                command=value
+            elif (op == "--mode"):
+                mode=value
             elif (op == "--src"):
                 src=value
             elif (op == "--dest"):
                 dest= value
+            elif (op == "--command"):
+                command=value
 
-    if command=='shell':
+    if mode=='do':
+        ssh_command(#host,command)
+    elif mode=='get':
+        get(#host,src)
+    elif mode=='put':
+        put(#host,src,dest)
+    elif mode=='show':
+        ####
 
-
-    # if choice[0]=='show':
-    #     print(dic.keys())
-    # elif choice[0] == 'get':
-    #     get(choice)
-    # elif choice[0] == 'put':
-    #     put(choice)
-    # else:
-    #     ssh_command(choice)
