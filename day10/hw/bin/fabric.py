b653d8fb59e845061537cee6ad0ce5748dfa2a48
ChampagneCui <xbcui2010@hotmail.com>
2017-02-02 16:44:30 +0800

    day10

--- day10/hw/bin/fabric.py
+++ day10/hw/bin/fabric.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 #_*_coding:utf-8_*_
 
 from sys import path
 path.append(r'../conf')
 path.append(r'../modules')
 from settings import *
-from features import feature
+from features import *
 import sys
 import getopt
 import threading
@@ -13,50 +13,36 @@ import threading
 def usage():
     pass
 
-def get_host_info(operation):
-    if group in operation.keys():
-        group_name = operation[group]
-        host_list = gconf.get(group_name, "host")
-        return host_list
-    elif host in operation.keys():
-        host_list = []
-        host = operation[host]
-        host_list.append(host)
-        return host_list
-    else:
-        return 1 ##报错
+
 
 if __name__ == '__main__':
-    #fab --group=testgroup --mode="shell" --command="ls -al"
-    #fab --host=1.1.1.1 --mode="put" --src="/tmp" --dest="/tmp"
-    opts, args = getopt.getopt(sys.argv[1:],longopts =["help","group=","host=","mode=","src=","dest=","command="])
+    #fabric --group=testgroup --mode="shell" --command="ls -al"
+    #fabric --host=1.1.1.1 --mode="put" --src="/tmp" --dest="/tmp"
+    opts, args = getopt.getopt(sys.argv[1:],"h",["help","group=","host=","mode=","src=","dest=","command="])
     for op, value in opts:
             if (op == "--group"):
-                operation[group]=value
+                operation["group"]=value
             elif (op == "--host"):
-                operation[host]=value
+                operation["host"]=value
             elif (op == "--mode"):
-                operation[mode]=value
+                operation["mode"]=value
             elif (op == "--src"):
-                operation[src]=value
+                operation["src"]=value
             elif (op == "--dest"):
-                operation[dest]= value
+                operation["dest"]= value
             elif (op == "--command"):
-                operation[command]=value
+                operation["command"]=value
             else:
                 usage() #help说明
 
-    if hasattr(feature,operation[mode]):
-        func = getattr(feature, operation[mode])
+    if hasattr(feature,operation["mode"]):
+        func = getattr(feature, operation["mode"])
         i = 0
         host_list = get_host_info(operation)
         while i < len(host_list):
-            ip = hconf.get(host_list[i], "ip")
-            port = hconf.get(host_list[i], "port")
-            username = hconf.get(host_list[i], "username")
-            password = hconf.get(host_list[i], "password")
-            S= threading.Thread(target=func,args=(operation,ip,port,username,password,))
-            S.start()
+            # S= threading.Thread(target=func,args=(operation,ip,port,username,password,))
+            # S.start()
+            func(operation,i)
             i+=1
     else:
         print("doesn't support type.")
--- day10/hw/modules/features.py
+++ day10/hw/modules/features.py
@@ -6,38 +6,57 @@ from sys import path
 path.append(r'../conf')
 from settings import *
 import paramiko
-import ConfigParser
 
+def get_host_info(operation):
+    if "group" in operation.keys():
+        group_name = operation["group"]
+        host_list = gconf.get(group_name, "host")
+        return host_list
+    elif "host" in operation.keys():
+        host_list = []
+        host = operation["host"]
+        host_list.append(host)
+        return host_list
+    else:
+        return 1 ##报错
 
-
-
+def get_info(host):
+    ip = hconf.get(host, "ip")
+    port = hconf.get(host, "port")
+    username = hconf.get(host, "username")
+    password = hconf.get(host, "password")
+    return ip,port,username,password
 
 class feature:
     @staticmethod
-    def ssh_command(operation,ip,port,username,password):
-        transport = paramiko.Transport((ip,port ))
-        transport.connect(username=username,password=password )
+    def shell(operation,i):
+        host_list = get_host_info(operation)
+        ip,port,username,password=get_info(host_list[i])
         ssh = paramiko.SSHClient()
-        ssh._transport = transport
-        stdin, stdout, stderr = ssh.exec_command(operation[command])
-        print stdout.read()
-        transport.close()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=ip, port=port, username=username, password=password)
+        stdin, stdout, stderr = ssh.exec_command(operation["command"])
+        print(stdout.read())
+        ssh.close()
 
     @staticmethod
-    def get(operation,ip,port,username,password):
-        transport = paramiko.Transport((ip,port))
+    def get(operation,i):
+        host_list = get_host_info(operation)
+        ip, port, username, password = get_info(host_list[i])
+        transport = paramiko.Transport((ip,int(port)))
         transport.connect(username=username, password=password)
 
         sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.put(operation[src])
-
+        sftp.get(operation["src"])
         transport.close()
 
     @staticmethod
-    def put(operation,ip,port,username,password):
-        transport = paramiko.Transport((ip, port))
+    def put(operation,i):
+        host_list = get_host_info(operation)
+        ip, port, username, password = get_info(host_list[i])
+        transport = paramiko.Transport((ip, int(port)))
         transport.connect(username=username, password=password)
 
         sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.put(operation[src], operation[dest])
+        sftp.put(operation["src"], operation["dest"])
         transport.close()
