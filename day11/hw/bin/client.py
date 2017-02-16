5d0bf606d106c85a81e248c93f4eb3f83c8c6d73
root <root@ubuntu.members.linode.com>
2017-02-16 06:42:16 +0000

    day11

--- day11/hw/bin/client.py
+++ day11/hw/bin/client.py
@@ -1,20 +1,51 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 #_*_coding:utf-8_*_
 
 import socket
 import sys
-
+import json
 
 server_address = ('localhost', 10000)
 
 # Create a TCP/IP socket
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.connect(server_address)
 
-while True:
-    msg=bytes(raw_input(">>:"),encoding="utf-8")
-    s.sendall(msg)
-    data=s.recv(1024)
-    print(data)
-s.close()
+class feature:
+	@staticmethod
+	def ls(*args,**kwargs):
+        	msg_data = {'action': 'ls'}
+		c.send(bytes(json.dumps(msg_data)))
+		list=eval(s.recv(4096))
+		i=0
+		try:
+			while i < len(list):
+			print(list[i])
+			i+=1
+		except:
+			print('nothing!')
+
+def main():
+	while True:
+    		send_data=bytes(input(">>:"),encoding="utf-8")
+		if len(send_data)==0:continue
+		cmd_list=send_data.split()
+		task_type=cmd_list[0]
+		if len(cmd_list)<2 and task_type !='ls':continue
+		if hasattr(feature, task_type):
+        		func=getattr(feature, task_type)
+        		func(cmd_list)
+		else:
+			print("doesn't support type.")
+		continue
+    		#s.sendall(msg)
+    		#data=s.recv(1024)
+    		#print(data)
+	s.close()
+
+if __name__=='__main__':
+	server_address = ('localhost', 10000)
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(server_address)
+
+	main()
--- day11/hw/bin/select_ftp.py
+++ day11/hw/bin/select_ftp.py
@@ -1,35 +1,46 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 #_*_coding:utf-8_*_
 
+from sys import path
+path.append(r'../conf')
+path.append(r'../modules')
+from settings import *
+from classes import *
 import selectors
 import socket
 
 sel = selectors.DefaultSelector()
-
-def accept(sock, mask):
-    conn, addr = sock.accept()  # Should be ready
-    print('accepted', conn, 'from', addr)
-    conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, read)
-
-def read(conn, mask):
-    data = conn.recv(1000)  # Should be ready
-    if data:
-        print('echoing', repr(data), 'to', conn)
-        conn.send(data)  # Hope it won't block
-    else:
-        print('closing', conn)
-        sel.unregister(conn)
-        conn.close()
-
 sock = socket.socket()
 sock.bind(('localhost', 10000))
 sock.listen(100)
 sock.setblocking(False)
 sel.register(sock, selectors.EVENT_READ, accept)
 
+def accept(sock, mask):
+	conn, addr = sock.accept()  # Should be ready
+	print('accepted', conn, 'from', addr)
+	conn.setblocking(False)
+	sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn, mask):
+        recv_data = conn.recv(1000)  # Should be ready
+        if data:
+                print('echoing', repr(recv_data), 'to', conn)
+                data=json.loads(recv_data.decode())
+                action=data.get("action")
+                if hasattr(feature,action):
+                        func = getattr(feature, action)
+                        func(data,conn)
+                else:
+                        print("task action is not supported", action)
+        else:
+                print('closing', conn)
+                sel.unregister(conn)
+                conn.close()
+
+
 while True:
-    events = sel.select()
-    for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+	events = sel.select()
+	for key, mask in events:
+		callback = key.data
+		callback(key.fileobj, mask)
