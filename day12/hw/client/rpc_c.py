dc463005270ade28a13154e726048941dbf0d0cf
ChampagneCui <xbcui2010@hotmail.com>
2017-06-03 12:37:11 +0800

    day12

--- day12/hw/client/rpc_c.py
+++ day12/hw/client/rpc_c.py
@@ -20,14 +20,12 @@ class RpcClient(object):
 
         self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
 
-
-
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = json.loads(body)
 
     def call(self, n):
-        self.response = None
+        #self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='rpc_ex',
                                    routing_key='',
@@ -47,7 +45,6 @@ class RpcClient(object):
             self.connection.process_data_events()
             print("no msg")
             time.sleep(0.5)
-            #self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
         print(self.response)
 
 if __name__ == '__main__':
