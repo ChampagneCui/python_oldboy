8f4579c74fda8422f64270060f73e2e4f0011f47
SAICStack User <appuser@hygieia.novalocal>
2017-06-05 14:38:35 +0800

    day12

--- day12/hw/client/rpc_c.py
+++ day12/hw/client/rpc_c.py
@@ -7,6 +7,7 @@ import json
 import time
 
 operation={}
+res_dict={}
 
 class RpcClient(object):
     def __init__(self):
@@ -18,14 +19,15 @@ class RpcClient(object):
         result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
 
-        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+        self.channel.basic_consume(self.on_response, queue=self.callback_queue)
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
-            self.response = json.loads(body)
+            self.response=json.loads(body)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
     def call(self, n):
-        #self.response = None
+        self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='rpc_ex',
                                    routing_key='',
@@ -34,22 +36,25 @@ class RpcClient(object):
                                          correlation_id = self.corr_id,
                                          ),
                                    body=str(n))
-        #while self.response is None:
-            #self.connection.process_data_events()
-        ##return self.response
-        #print(self.response)
-
-    def get(self):
-        self.response = None
         while self.response is None:
             self.connection.process_data_events()
-            print("no msg")
-            time.sleep(0.5)
-        print(self.response)
+        return self.response
+
+    def on_res(self, num,ch, method, props, body):
+        if num == props.correlation_id:
+            self.res_response=json.loads(body)
+
+
+    def get(self,n):
+        self.res_response= None
+        self.channel.basic_consume(self.on_res(num=n), queue=self.callback_queue, no_ack=True,)
+        while self.res_response is None:
+            self.connection.process_data_events()
+        return self.res_response
 
 if __name__ == '__main__':
     rpc = RpcClient()
-    opts, args = getopt.getopt(sys.argv[1:],"h",["help","host=","command=","check"])
+    opts, args = getopt.getopt(sys.argv[1:],"h",["help","host=","command=","check="])
     for op, value in opts:
             if (op == "--host"):
                 value=value.split()
@@ -57,12 +62,13 @@ if __name__ == '__main__':
             elif (op == "--command"):
                 operation["command"]=value
             elif (op == "--check"):
-                rpc.get()
+                a=rpc.get(value)
+                print(a)
             else:
                 print("run --command='df -h' --hosts=192.168.3.55 10.4.3.4") #help说明
                 exit()
 
-    print(" [x] Requesting operation")
-    #response = rpc.call(json.dumps(operation))
-    rpc.call(json.dumps(operation))
-    #print(" [.] Got %r" % response)
+    #print(" [x] Requesting operation")
+    response = rpc.call(json.dumps(operation))
+    print(" [.] task num: %r" % response)
+
--- day12/hw/server/rpc_s.py
+++ day12/hw/server/rpc_s.py
@@ -4,7 +4,10 @@ import json
 import subprocess
 import socket
 import time
+import random
+import uuid
 
+response_dict={}
 
 def get_ip():
     hostname = socket.gethostname()
@@ -12,24 +15,39 @@ def get_ip():
     return ip
 
 def doing(operation):
-    output=subprocess.check_output(operation["command"], shell=True) 
+    try:
+        output=subprocess.check_output(operation["command"], shell=True)
+    except:
+        output="error"
     return output
 
 def on_request(ch, method, props, body):
     operation = json.loads(body)
-    if ip in operation["host"]:
-        response = doing(operation)
-        print(response)
+    if operation.has_key("host") and operation.has_key("command"):
+        if ip in operation["host"]:
+            new_corr_id = random.randint(0,99999)
 
-        ch.basic_publish(exchange='',
-                         routing_key=props.reply_to,
-                         properties=pika.BasicProperties(correlation_id = \
-                                                             props.correlation_id),
-                         body=json.dumps(response))
-        ch.basic_ack(delivery_tag = method.delivery_tag)
+            ch.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(correlation_id=props.correlation_id,),
+                             body=json.dumps(new_corr_id)
+                             )
+
+            response = doing(operation)
+            response_dict[new_corr_id] = response
+            print(response)
+            time.sleep(1)
+
+            ch.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(correlation_id=new_corr_id,),
+                             body=json.dumps(response_dict[new_corr_id])
+                             )
+
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     ip=get_ip()
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
@@ -38,6 +56,7 @@ if __name__=='__main__':
     queue_name = result.method.queue
     channel.queue_bind(exchange='rpc_ex',queue=queue_name)
     channel.basic_consume(on_request, queue=queue_name)
+
 
     print(" [x] Awaiting RPC requests")
     channel.start_consuming()
