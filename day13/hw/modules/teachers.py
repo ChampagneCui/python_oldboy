268acc8c7209fea15d5ed1f0d7cdce25ed2dffa1
Champagne <xbcui2010@hotmail.com>
2017-06-12 16:54:46 +0800

    day13

--- day13/hw/modules/teachers.py
+++ day13/hw/modules/teachers.py
@@ -16,5 +16,15 @@ class User(Base):
 	name = Column(String(32))
 	password = Column(String(64))
 
-
 Base.metadata.create_all(engine)  # 创建表结构
+
+Session_class = sessionmaker(bind=engine)  # 创建与数据库的会话session class ,注意,这里返回给session的是个class,不是实例
+Session = Session_class()  # 生成session实例
+
+user_obj = User(name="alex", password="alex3714")  # 生成你要创建的数据对象
+print(user_obj.name, user_obj.id)  # 此时还没创建对象呢，不信你打印一下id发现还是None
+
+Session.add(user_obj)  # 把要创建的数据对象添加到这个session里， 一会统一创建
+print(user_obj.name, user_obj.id)  # 此时也依然还没创建
+
+Session.commit()  # 现此才统一提交，创建数据
