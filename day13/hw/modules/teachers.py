4890de9ed15eca363e141de5c678a5351f62a1ca
root <root@hygieia.novalocal>
2017-06-12 16:46:29 +0800

    day13

--- day13/hw/modules/teachers.py
+++ day13/hw/modules/teachers.py
@@ -1,9 +1,10 @@
+#_*_coding:utf-8_*_
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-engine = create_engine("mysql+pymysql://root:Pass12344@localhost/course",
+engine = create_engine("mysql+pymysql://root:Pass1234@127.0.0.1/course",
 					   encoding='utf-8', echo=True)
 
 Base = declarative_base()  # 生成orm基类
@@ -11,7 +12,7 @@ Base = declarative_base()  # 生成orm基类
 
 class User(Base):
 	__tablename__ = 'user'  # 表名
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer,auto ,primary_key=True)
 	name = Column(String(32))
 	password = Column(String(64))
 
