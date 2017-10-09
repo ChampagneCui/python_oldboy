1fa0bc87c75b5ce6040d494e45f8b28a50cf6a6c
Chamapgne <xbcui2010@hotmail.com>
2017-10-09 22:45:49 +0800

    day16

--- day16/hw/common.js
+++ day16/hw/common.js
@@ -1,19 +1,53 @@
 function login() {
-    document.getElementById("i1").classList.remove('hide')
-    document.getElementById("i2").classList.remove('hide')
+    document.getElementById("i1").classList.remove('hide');
+    document.getElementById("i2").classList.remove('hide');
 }
 
 function register() {
-    document.getElementById("i1").classList.remove('hide')
-    document.getElementById("i3").classList.remove('hide')
+    document.getElementById("i1").classList.remove('hide');
+    document.getElementById("i3").classList.remove('hide');
 }
 
 function rm_login() {
-    document.getElementById("i2").classList.add('hide')
-    document.getElementById("i1").classList.add('hide')
+    document.getElementById("i2").classList.add('hide');
+    document.getElementById("i1").classList.add('hide');
 }
 
 function rm_regiter() {
-    document.getElementById("i3").classList.add('hide')
-    document.getElementById("i1").classList.add('hide')
+    document.getElementById("i3").classList.add('hide');
+    document.getElementById("i1").classList.add('hide');
+}
+
+function checkedall() {
+    var tbody=document.getElementById('car');
+    var tr_list=tbody.children;
+    for (var i=0;i<tr_list.length;i++) {
+        var current_tr=tr_list[i];
+        var checkbox=current_tr.children[0];
+        checkbox.checked=true;
+    }
+}
+
+function cancelall() {
+    var tbody=document.getElementById('car');
+    var tr_list=tbody.children;
+    for (var i=0;i<tr_list.length;i++) {
+        var current_tr=tr_list[i];
+        var checkbox=current_tr.children[0];
+        checkbox.checked=false;
+    }
+}
+
+function reverseall() {
+    var tbody=document.getElementById('car');
+    var tr_list=tbody.children;
+    for (var i=0;i<tr_list.length;i++) {
+        var current_tr=tr_list[i];
+        var checkbox=current_tr.children[0];
+        if(checkbox.checked==true) {
+            checkbox.checked = false;
+        }
+        else
+            checkbox.checked=true;
+    }
 }
